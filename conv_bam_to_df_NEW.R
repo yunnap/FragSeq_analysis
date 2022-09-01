6090785a74a5c810aa98ea861d94cf5b5fcc8353
Yunna Petrusenko <107843906+yunnap@users.noreply.github.com>
2022-09-01 14:44:18 +0300

    Add files via upload
    how to launch
    
    R CMD BATCH --vanilla "--args induction_res_FragSeq/Results_fragseq_rep1/Fraq_Seq_S16_S16_R1_001_R2_combi_ref_proper_mapped.bam induction_res_FragSeq/Results_fragseq_rep1/Fraq_Seq_S16_S16_R1_001_R2_combi_ref_proper_mapped.csv" conv_bam_to_df_NEW.R

---
+++ conv_bam_to_df_NEW.R
@@ -0,0 +1,43 @@
+myargs = commandArgs(trailingOnly=TRUE)
+path_to_bam = myargs[1]
+path_to_csv = myargs[2]
+
+
+#load library
+library(Rsamtools)
+#read in entire BAM file
+#bam <- scanBam("/Users/unnost/workwork/prespacers_task/trimmomatic_exp_4/unique.bam")
+#bam <- scanBam("/Users/unnost/workwork/prespacers_task/probnaya/Results_fragseq/Oct14_S10_R1_001_R2_largefile_uni_mapped.bam")
+#bam <- scanBam("/Users/unnost/workwork/prespacers_task/Results_fragseq/Oct14_S10_R1_001_R2_kd403-real_uni_mapped.bam")
+
+#bam <- scanBam("/Users/unnost/workwork/prespacers_task/probnaya/Results_fragseq/filtered_bamfile.bam")
+
+
+
+bam <- scanBam(path_to_bam)
+
+#names of the BAM fields
+names(bam[[1]])
+#distribution of BAM flags
+table(bam[[1]]$flag)
+.unlist <- function (x){
+    ## do.call(c, ...) coerces factor to integer, which is undesired
+    x1 <- x[[1L]]
+    if (is.factor(x1)){
+       structure(unlist(x), class = "factor", levels = levels(x1))
+    } else {
+       do.call(c, x)
+    }
+ }
+#store names of BAM fields
+bam_field <- names(bam[[1]])
+#go through each BAM field and unlist
+list <- lapply(bam_field, function(y) .unlist(lapply(bam, "[[", y)))
+#store as data frame
+bam_df <- do.call("DataFrame", list)
+names(bam_df) <- bam_field
+dim(bam_df)
+#View(bam_df)
+write.table(bam_df, path_to_csv )
+#head(bam_df)
+
